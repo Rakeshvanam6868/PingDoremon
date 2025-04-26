0596032db0719de7434cec02e762d79ebc8e04f1
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-04-26 18:31:17 +0530

    Resolved the stripe appVersion

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2025-04-26",
+  apiVersion: "2022-11-15",
   typescript: true,
 })
 
