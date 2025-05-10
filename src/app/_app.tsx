3e09f65ec03d7201a037deea747ac9ec78c32207
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-05-10 16:44:43 +0530

    pnpm i done

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,9 +1,11 @@
-// app/_app.tsx
+// src/app/_app.tsx
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { AppProps } from 'next/app'
 
 const queryClient = new QueryClient()
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
